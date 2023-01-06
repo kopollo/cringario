89f8a66843a84fd8c3c179512387d3953b61a5b1
11kara11 <beev.kirill@gmail.com>
2023-01-06 13:10:10 +0300

    Test buttons

--- window_objects/windows.py
+++ window_objects/windows.py
@@ -6,6 +6,7 @@ from drawable import DrawWithSprite, DrawWithColor
 from tiles import Tile
 from levels.test_level import platform_size
 from bonuses import HealBonus
+import buttons as button
 
 
 class BaseWindow(ABC):
@@ -24,3 +25,12 @@ class StartWindow(BaseWindow):
     def render(self):
         self.menu.draw(self.display)
         pass
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+start_img = pygame.image.load('').convert_alpha()
+exit_img = pygame.image.load('').convert_alpha()
+result_table_img = pygame.image.load('').convert_alpha()
+start_button = button.Button(100, 200, start_img, 0.8)
+exit_button = button.Button(450, 200, exit_img, 0.8)
+resultbutton = button.Button(450, 200, result_table_img, 0.8)
